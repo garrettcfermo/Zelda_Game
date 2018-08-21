7e72956c58c2155210fff0d615f39e49e66a31fd
Garrett Fermo <garrettcfermo@gmail.com>
2018-08-20 18:11:33 -0700

    Instruction HTML/CSS/JS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,26 @@
 
-// Start Menu 
+// Start Menu
+
+// Start Game Button
 $('.start-btn').on('click',function(){
   $('.start-menu').css('display','none')
+})
+
+// Instruction Game Button
+$('.inst-btn').on('click', function () {
+  $('.Instructions').css('display', 'block')
+  $('.start-menu').css('display', 'none')
+})
+
+// Instruction 
+
+// Start Game Instruction Button
+$('.start-inst-btn').on('click', function () {
+  $('.Instructions').css('display', 'none')
+})
+
+// Back Button
+$('.back-btn').on('click', function () {
+  $('.Instructions').css('display', 'none')
+  $('.start-menu').css('display', 'block')
 })
